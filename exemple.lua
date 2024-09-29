6876fc5068c22e4960579e5d0e1e90efbb059e48
JaiLaDalleDeOufMgl <147735499+JaiLaDalleDeOufMgl@users.noreply.github.com>
2024-09-29 20:46:32 +0200

    Update exemple

--- exemple.lua
+++ exemple.lua
@@ -1,6 +1,6 @@
 local playerManager = PlayerManager
 
-RegisterCommand("solde", function(source, args, rawCommand)
+RegisterCommand("balance", function(source, args, rawCommand)
     local identifier = GetPlayerIdentifiers(source)[1]
     local currency = args[1] or Config.DefaultCurrency
 
@@ -10,9 +10,9 @@ RegisterCommand("solde", function(source, args, rawCommand)
     TriggerClientEvent('chat:addMessage', source, { args = { "[Banque]", "Solde en " .. currency .. ": " .. balance .. " " .. Config.Currencies[currency].symbol } })
 end)
 
-RegisterCommand("donner", function(source, args, rawCommand)
+RegisterCommand("transfer", function(source, args, rawCommand)
     local fromIdentifier = GetPlayerIdentifiers(source)[1]
-    local toPlayerId = tonumber(args[1])s
+    local toPlayerId = tonumber(args[1])
     local amount = tonumber(args[2])
     local currency = args[3] or Config.DefaultCurrency
 
@@ -20,13 +20,9 @@ RegisterCommand("donner", function(source, args, rawCommand)
 
     if toIdentifier then
         playerManager:transferMoney(fromIdentifier, toIdentifier, currency, amount)
-        if Config.Chat then
-            TriggerClientEvent('chat:addMessage', source, { args = { "[Banque]", "Vous avez donné " .. amount .. " " .. Config.Currencies[currency].symbol .. " à " .. GetPlayerName(toPlayerId) } })
-            TriggerClientEvent('chat:addMessage', toPlayerId, { args = { "[Banque]", "Vous avez reçu " .. amount .. " " .. Config.Currencies[currency].symbol .. " de " .. GetPlayerName(source) } })
-        end
+        TriggerClientEvent('chat:addMessage', source, { args = { "[Banque]", "Vous avez donné " .. amount .. " " .. Config.Currencies[currency].symbol .. " à " .. GetPlayerName(toPlayerId) } })
+        TriggerClientEvent('chat:addMessage', toPlayerId, { args = { "[Banque]", "Vous avez reçu " .. amount .. " " .. Config.Currencies[currency].symbol .. " de " .. GetPlayerName(source) } })
     else
-        if Config.Chat then
-            TriggerClientEvent('chat:addMessage', source, { args = { "[Banque]", "Joueur introuvable." } })
-        end
+        TriggerClientEvent('chat:addMessage', source, { args = { "[Banque]", "Joueur introuvable." } })
     end
 end)
